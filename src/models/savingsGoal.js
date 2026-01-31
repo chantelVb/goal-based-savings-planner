bc74bb7a49a69dac88236ca33d630db70ee127e7
Chantel Boiro <97921215+chantelVb@users.noreply.github.com>
2026-01-31 23:14:08 +0000

    Folder structure and goal creation logic added (#1)

---
+++ src/models/savingsGoal.js
@@ -0,0 +1,46 @@
+'use strict'
+
+// a single data shape for savings goal
+
+const savingsGoalData = {
+    goal_one: {
+        savingsGoal: " ",       // (*)
+        reasonForGoal: " ",     // (opt)
+        goalAmount: 0,        // (*)
+        desiredTimeframe: 0,  // years months // (*)
+        deposit: 0,           // weekly/monthly deposit // (*)
+        actualTimeframe: 0,   // DT/D = ATF years months // (*)
+    },
+    goal_two: {
+        savingsGoal: " ",       // (*)
+        reasonForGoal: " ",     // (opt)
+        goalAmount: 0,        // (*)
+        desiredTimeframe: 0,  // years months // (*)
+        deposit: 0,           // weekly/monthly deposit // (*)
+        actualTimeframe: 0,   // DT/D = ATF years months // (*)
+    },
+    goal_three: {
+        savingsGoal: " ",       // (*)
+        reasonForGoal: " ",     // (opt)
+        goalAmount: 0,        // (*)
+        desiredTimeframe: 0,  // years months // (*)
+        deposit: 0,           // weekly/monthly deposit // (*)
+        actualTimeframe: 0,   // DT/D = ATF years months // (*)
+    },
+    goal_four: {
+        savingsGoal: " ",       // (*)
+        reasonForGoal: " ",     // (opt)
+        goalAmount: 0,        // (*)
+        desiredTimeframe: 0,  // years months // (*)
+        deposit: 0,           // weekly/monthly deposit // (*)
+        actualTimeframe: 0,   // DT/D = ATF years months // (*)
+    },
+    goal_five: {
+        savingsGoal: " ",       // (*)
+        reasonForGoal: " ",     // (opt)
+        goalAmount: 0,        // (*)
+        desiredTimeframe: 0,  // years months // (*)
+        deposit: 0,           // weekly/monthly deposit // (*)
+        actualTimeframe: 0,   // DT/D = ATF years months // (*)
+    },
+}
